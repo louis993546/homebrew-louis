b9c7c4e3ed625917f1803ae50ca805d9d87c3b40
Louis <louis993546@gmail.com>
2023-04-16 10:37:32 +0200

    Fix version

--- Casks/launchpadder.rb
+++ Casks/launchpadder.rb
@@ -1,5 +1,5 @@
 cask "launchpadder" do
-  version "1681633172"
+  version "1.4"
   sha256 "e6f22aec28546b3ab418cb06c9a26c696126c4ec4daeeaebd1a427a8c8b86390"
 
   url "https://molayc.com/blog/download/launchpadder-v1-4/?wpdmdl=711&refresh=643baf94bf7dc#{version}"
