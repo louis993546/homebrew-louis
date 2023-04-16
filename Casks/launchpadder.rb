1418aadf81c793b1c8342ed305370bb2805aa03a
Louis <louis993546@gmail.com>
2023-04-16 11:02:38 +0200

    Add version back in

--- Casks/launchpadder.rb
+++ Casks/launchpadder.rb
@@ -2,7 +2,7 @@ cask "launchpadder" do
   version "1.4"
   sha256 "e6f22aec28546b3ab418cb06c9a26c696126c4ec4daeeaebd1a427a8c8b86390"
 
-  url "https://molayc.com/blog/download/launchpadder-v1-4/?wpdmdl=711"
+  url "https://molayc.com/blog/download/launchpadder-v#{version.tr!('.', '-')}/?wpdmdl=711"
   name "LaunchPadder"
   desc "Organizes and sorts apps and folders on LaunchPad according to certain rules"
   homepage "https://molayc.com/blog/2022/04/07/launchpadder/"
