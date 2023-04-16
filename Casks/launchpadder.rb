8a8adfddfb0a6b81dc66582a7642ff3570eb7ee5
Louis <louis993546@gmail.com>
2023-04-16 10:40:55 +0200

    Simplify URL

--- Casks/launchpadder.rb
+++ Casks/launchpadder.rb
@@ -2,7 +2,7 @@ cask "launchpadder" do
   version "1.4"
   sha256 "e6f22aec28546b3ab418cb06c9a26c696126c4ec4daeeaebd1a427a8c8b86390"
 
-  url "https://molayc.com/blog/download/launchpadder-v1-4/?wpdmdl=711&refresh=643baf94bf7dc#{version}"
+  url "https://molayc.com/blog/download/launchpadder-v1-4/?wpdmdl=711"
   name "LaunchPadder"
   desc "a free macOS LaunchPad tool that organizes and sorts apps and folders on LaunchPad according to certain rules."
   homepage "https://molayc.com/blog/2022/04/07/launchpadder/"
